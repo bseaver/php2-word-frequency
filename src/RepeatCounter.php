f61482aafd06a352e20ca35e1e1d8acc7fb084f2
Benjamin T. Seaver <benjamintseaver@gmail.com>
2017-02-17 08:55:25 -0800

    Pass next test (src/RepeatCounter.php)

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -3,9 +3,16 @@
     {
         function countRepeats($search_word, $string_to_search)
         {
-            if ($search_word == $string_to_search) {
-                return 1;
+            $count = 0;
+            $string_to_search_words = explode(' ', $string_to_search);
+
+            foreach ($string_to_search_words as $string_to_search_word) {
+                if ($search_word == $string_to_search_word) {
+                    $count++;
+                }
             }
+
+            return $count;
         }
     }
 ?>
