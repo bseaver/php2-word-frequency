8982bc25f4e1fbe52ea6647e2bff4f1202edc992
Benjamin T. Seaver <benjamintseaver@gmail.com>
2017-02-17 08:31:16 -0800

    Add first test case (tests/RepeatCounterTest.php)

---
+++ tests/RepeatCounterTest.php
@@ -0,0 +1,29 @@
+<?php
+    require_once "src/RepeatCounter.php";
+
+    class RepeatCounterTest extends PHPUnit_Framework_TestCase
+    {
+        function test_countRepeats()
+        {
+            // Arrange
+            $repeatCounter_class = new RepeatCounter();
+
+            // Specs from README.md:
+            // _String_ to process, _Word_ to Count, _Count_, _Reasoning_ for Test
+            $expected_results = array(
+                array('string' => 'start', 'word' => 'start', 'count' => 1, 'reasoning' => 'Most basic and will fail first')
+            );
+
+            foreach ($expected_results as $expected_result) {
+                // Arrange
+                $actual_result = $expected_result;
+
+                // Act
+                $actual_result['count'] = $repeatCounter_class->countRepeats($actual_result['word'], $actual_result['string']);
+
+                // Assert
+                $this->assertEquals($expected_result, $actual_result);
+            }
+        }
+    }
+?>
