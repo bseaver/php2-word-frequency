888155d599d1a6bb54489c7fc06dcec54fac6e81
Benjamin T. Seaver <benjamintseaver@gmail.com>
2017-02-17 12:23:44 -0800

    Add Twig template on home post route (app/app.php, views/word_frequency.html.twig)

--- app/app.php
+++ app/app.php
@@ -1,6 +1,7 @@
 <?php
     date_default_timezone_set('America/Los_Angeles');
     require_once __DIR__.'/../vendor/autoload.php';
+    require_once __DIR__.'/../src/RepeatCounter.php';
 
     $app = new Silex\Application();
     $app['debug'] = true;
@@ -12,6 +13,26 @@
 
     $app->get('/', function() use ($app) {
         return $app['twig']->render('word_frequency.html.twig');
+    });
+
+    $app->post('/', function() use ($app) {
+        $search_word = $_POST['search_word'];
+        $string_to_search = $_POST['string_to_search'];
+        $word_frequency = '';
+
+        if ($search_word && $string_to_search) {
+            $newRepeatCounter = new RepeatCounter;
+            $word_frequency = (string) $newRepeatCounter->countRepeats($search_word, $string_to_search);
+        }
+
+        return $app['twig']->render(
+            'word_frequency.html.twig',
+            array(
+                'search_word' => $search_word,
+                'string_to_search' => $string_to_search,
+                'word_frequency' => $word_frequency
+            )
+        );
     });
 
     return $app;
