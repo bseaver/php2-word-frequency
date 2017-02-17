5bcf9c3621df0e80bac2d6153ff69aa6792020bf
Benjamin T. Seaver <benjamintseaver@gmail.com>
2017-02-17 09:46:36 -0800

    Pass 5th test (src/RepeatCounter.php)

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -3,7 +3,24 @@
     {
         function countRepeats($search_word, $string_to_search)
         {
+            $a_space = ' ';
+            $empty_string = '';
             $count = 0;
+
+            // Change all non-alpha numeric characters in string to search to spaces
+            $string_to_search_characters = str_split($string_to_search);
+            array_walk(
+                $string_to_search_characters,
+                function (&$value, $key, $a_space)
+                {
+                    if (!ctype_alnum($value)) {
+                        $value = $a_space;
+                    }
+                },
+                $a_space
+            );
+            $string_to_search = join($string_to_search_characters, $empty_string);
+
             $search_word = strtoupper($search_word);
             $string_to_search_words = explode(' ', strtoupper($string_to_search));
 
