ae529c53b38383378c16f430cfd1afa29f1ed53e
Benjamin T. Seaver <benjamintseaver@gmail.com>
2017-02-17 09:14:05 -0800

    Pass 4th test (src/RepeatCounter.php)

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -4,7 +4,8 @@
         function countRepeats($search_word, $string_to_search)
         {
             $count = 0;
-            $string_to_search_words = explode(' ', $string_to_search);
+            $search_word = strtoupper($search_word);
+            $string_to_search_words = explode(' ', strtoupper($string_to_search));
 
             foreach ($string_to_search_words as $string_to_search_word) {
                 if ($search_word == $string_to_search_word) {
